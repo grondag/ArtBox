84a59c504221792a10378b13efd96ec1f27c51a5
grondag <grondag@gmail.com>
2018-10-08 07:39:06 -0700

    project setup

---
+++ src/main/java/grondag/artbox/ServerProxy.java
@@ -0,0 +1,10 @@
+package grondag.artbox;
+
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+@SideOnly(Side.SERVER)
+public class ServerProxy extends CommonProxy
+{
+	
+}
